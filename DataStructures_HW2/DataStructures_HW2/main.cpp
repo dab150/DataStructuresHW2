3e19274cf19169c361718f4b71d4f71688105ac4
dab150 <cironid12@gmail.com>
2016-10-03 20:28:56 -0400

    BackToFront Traversal is pretty much working. Now to implement FrontToBack

--- DataStructures_HW2/DataStructures_HW2/main.cpp
+++ DataStructures_HW2/DataStructures_HW2/main.cpp
@@ -17,14 +17,15 @@ vector<int> steps;
 
 //function to click "lights" that are currently lit
 //the goal is to "push" all of the lights to the left
-void clickLight(vector<int> inputArray, int n)
+//this operates from back to front
+void traverseBackToFront(vector<int> inputArray, int n)
 {
 	if (n >= 0)
 	{
 		if (inputArray[n] == 0)
 		{
 			//ignore this light and call the function again with the next number down
-			clickLight(inputArray, n - 1);
+			traverseBackToFront(inputArray, n - 1);
 		}
 		else if (inputArray[n] == 1)
 		{
@@ -33,7 +34,7 @@ void clickLight(vector<int> inputArray, int n)
 			//if it is a 1 we should recursively call this function again
 			if (n > 0 && n < rowLength - 1) //not at the first or last element
 			{
-				if (inputArray[n - 1] == 0)// || inputArray[n + 1] == 1)
+				if (inputArray[n - 1] == 0 || inputArray[n + 1] == 1)
 				{
 					//"click" light then call function
 					inputArray[n] = (inputArray[n] + 1) % 2;
@@ -44,25 +45,11 @@ void clickLight(vector<int> inputArray, int n)
 					//change the light to the left (we know we aren't on the first light)
 					inputArray[n - 1] = (inputArray[n - 1] + 1) % 2;
 
-					clickLight(inputArray, n - 1);
+					traverseBackToFront(inputArray, n - 1);
 				}
 				else
 					//if (inputArray[n - 1] == 0 || inputArray[n - 1] == 1)
-					clickLight(inputArray, n - 1);
-			}
-			else if (n == 0) //we are at the first light
-			{
-				//"click" light
-				inputArray[n] = (inputArray[n] + 1) % 2;
-				//add this buttons index to our list of steps
-				steps.push_back(n + 1);
-				//change the light to the right of this one unless we are on the last light
-				//notice we don't change the light to the left because there isn't one since we are at index 0
-				if (n < rowLength - 1)
-					inputArray[n + 1] = (inputArray[n + 1] + 1) % 2;
-
-				//call function again
-				clickLight(inputArray, n - 1);
+					traverseBackToFront(inputArray, n - 1);
 			}
 			else if (n == rowLength - 1) //we are the last light
 			{
@@ -77,13 +64,38 @@ void clickLight(vector<int> inputArray, int n)
 					if (n > 0)
 						inputArray[n - 1] = (inputArray[n - 1] + 1) % 2;
 					//call function again
-					clickLight(inputArray, n - 1);
+					traverseBackToFront(inputArray, n - 1);
 				}
 				else
 				{
-					clickLight(inputArray, n - 1);
+					traverseBackToFront(inputArray, n - 1);
+				}
+			}
+			else if (n == 0) //we are at the first light
+			{
+				//"click" light
+				inputArray[n] = (inputArray[n] + 1) % 2;
+				//add this buttons index to our list of steps
+				steps.push_back(n + 1);
+				//change the light to the right of this one unless we are on the last light
+				//notice we don't change the light to the left because there isn't one since we are at index 0
+				if (n < rowLength - 1)
+					inputArray[n + 1] = (inputArray[n + 1] + 1) % 2;
+
+
+
+				//check to see if a 1 is still in the light row
+				//if there is, traverse the row from fron to back
+				for (int i = 0; i < rowLength; i++)
+				{
+					if (inputArray[i] == 1)
+					{
+						//traverseFrontToBack(inputArray, rowLength - 1);
+					}
 				}
 
+				//else call function again to exit
+				traverseBackToFront(inputArray, n - 1);
 			}
 		}
 	}
@@ -125,7 +137,7 @@ int main()
 			lightRow[i] = 0;
 	}
 
-	clickLight(lightRow, rowLength - 1);
+	traverseBackToFront(lightRow, rowLength - 1);
 
 	cout << "\n \n \nPress enter to exit...";
 	cin.get();
